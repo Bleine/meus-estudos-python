a9d156795223142f7f49e9cfcc4f5597037fd0a8
Bleine <bleinee@gmail.com>
2025-06-07 17:58:33 -0300

    Adiciona exercício de cálculo de investimento total

---
+++ investimentos.py
@@ -0,0 +1,7 @@
+valores_investidos = [1000, 2500, 3000, 1500, 500]
+investimento_total = 0
+
+for valor in valores_investidos:
+    investimento_total += valor
+
+print("Total investido:", investimento_total)
